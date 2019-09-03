cfc53a4bb210a467a1d8545c094ec6d8ba3ec9db
Cody Snider <cody@codysnider.com>
2019-09-03 00:49:15 -0400

    bash script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-docker build -f .\Dockerfile.php56 -t php56:latest .\
-docker build -f .\Dockerfile.php71 -t php71:latest .\
-docker build -f .\Dockerfile.php72 -t php72:latest .\
-docker build -f .\Dockerfile.php73 -t php73:latest .\
+docker build -f Dockerfile.php56 -t php56:latest .
+docker build -f Dockerfile.php71 -t php71:latest .
+docker build -f Dockerfile.php72 -t php72:latest .
+docker build -f Dockerfile.php73 -t php73:latest .
 
 docker run php56:latest
 docker run php71:latest
