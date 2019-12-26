e50d67d979176ae797076b9a29c5343f500d41b0
Cody Snider <cody@codysnider.com>
2019-12-26 14:55:16 -0500

    added 7.4 and partial build of 8

--- run.sh
+++ run.sh
@@ -4,8 +4,10 @@ docker build -f Dockerfile.php56 -t php56:latest .
 docker build -f Dockerfile.php71 -t php71:latest .
 docker build -f Dockerfile.php72 -t php72:latest .
 docker build -f Dockerfile.php73 -t php73:latest .
+docker build -f Dockerfile.php74 -t php74:latest .
 
 docker run php56:latest
 docker run php71:latest
 docker run php72:latest
 docker run php73:latest
+docker run php74:latest
